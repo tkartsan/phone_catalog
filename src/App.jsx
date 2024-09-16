fb1cae67b65309f17cdc4f3014c575901d203845
DESKTOP-JGJSHGL\bih <peroxide614@gmail.com>
2024-09-16 22:01:24 +0300

    modify App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,42 @@ import './App.css';
 
 import { CategoriesPick } from './components/CategoriesPick';
 import { CoverSlider } from './components/CoverSlider';
+import { DiscountModelsSlider } from './components/DiscountModelsSlider';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { NewModelsSlider } from './components/NewModelsSlider';
 import { useFetch } from './hooks/useFetch';
 import { getMostRecentPhones } from './utils/getMostRecentPhones';
+import { getTopDiscountedPhones } from './utils/getTopDiscountedPhones';
 
 function App() {
-  const { data: phonesJson } = useFetch('/api/phones.json');
-  const { data: tabletsJson } = useFetch('/api/tablets.json');
-  const { data: accessoriesJson } = useFetch('/api/accessories.json');
-  const { data: productsJson } = useFetch('/api/products.json');
+  const { data: phonesData } = useFetch('/api/phones.json');
+  const { data: tabletsData } = useFetch('/api/tablets.json');
+  const { data: accessoriesData } = useFetch('/api/accessories.json');
+  const { data: productsData } = useFetch('/api/products.json');
 
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-col gap-8 pt-[100px]">
         <CoverSlider />
-        {phonesJson && productsJson ? (
+        {phonesData && productsData ? (
           <NewModelsSlider
-            phones={getMostRecentPhones(phonesJson, productsJson)}
+            phones={getMostRecentPhones(phonesData, productsData)}
           />
         ) : (
           <p>Loading...</p>
         )}
         <CategoriesPick
-          phones={phonesJson}
-          tablets={tabletsJson}
-          accessories={accessoriesJson}
+          phones={phonesData}
+          tablets={tabletsData}
+          accessories={accessoriesData}
         />
+        {phonesData && productsData ? (
+          <DiscountModelsSlider phones={getTopDiscountedPhones(phonesData)} />
+        ) : (
+          <p>Loading...</p>
+        )}
       </main>
       <Footer />
     </div>
