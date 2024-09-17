40a578d7e64d521e930531d7b17822f515b546ee
DESKTOP-JGJSHGL\bih <peroxide614@gmail.com>
2024-09-17 21:12:06 +0300

    add proper className format

--- src/components/MobilePhones/MobilePhones.jsx
+++ src/components/MobilePhones/MobilePhones.jsx
@@ -5,6 +5,8 @@ import ReactPaginate from 'react-paginate';
 
 import { PhoneCard } from '../PhoneCard';
 
+import homeIcon from './../../assets/home.svg';
+
 export const MobilePhones = ({ phones }) => {
   const [currentPage, setCurrentPage] = useState(0);
   const phonesPerPage = 16;
@@ -16,12 +18,10 @@ export const MobilePhones = ({ phones }) => {
   const phoneCount = phones.length;
   const totalPages = Math.ceil(phoneCount / phonesPerPage);
 
-  // Get current phones for the page
   const indexOfLastPhone = (currentPage + 1) * phonesPerPage;
   const indexOfFirstPhone = indexOfLastPhone - phonesPerPage;
   const currentPhones = phones.slice(indexOfFirstPhone, indexOfLastPhone);
 
-  // Handle page change
   const handlePageChange = (data) => {
     setCurrentPage(data.selected);
   };
@@ -31,31 +31,29 @@ export const MobilePhones = ({ phones }) => {
       <div className="title">Mobile Phones</div>
       <p className="subtitle">{phoneCount} models</p>
 
-      {/* Display phones */}
       <div className="phone-grid">
         {currentPhones.map((phone) => (
           <PhoneCard key={phone.id} phone={phone} isShowDiscount={true} />
         ))}
       </div>
 
-      {/* Pagination */}
       <div className="pagination-container">
         <ReactPaginate
-          previousLabel={'<'}
-          nextLabel={'>'}
-          breakLabel={''}
+          previousLabel="<"
+          nextLabel=">"
+          breakLabel=""
           pageCount={totalPages}
           marginPagesDisplayed={0}
           pageRangeDisplayed={totalPages}
           onPageChange={handlePageChange}
-          containerClassName={'pagination'}
-          activeClassName={'active-page'}
-          pageClassName={'page'}
-          pageLinkClassName={'page-link'}
-          previousClassName={'page-prev'}
-          nextClassName={'page-next'}
-          disabledClassName={'disabled'}
-          breakClassName={'hidden'}
+          containerClassName="pagination"
+          activeClassName="active-page"
+          pageClassName="page"
+          pageLinkClassName="page-link"
+          previousClassName="page-prev"
+          nextClassName="page-next"
+          disabledClassName="disabled"
+          breakClassName="hidden"
         />
       </div>
     </div>
