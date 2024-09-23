3727ce4e09a7d6f8d034967367f7c21b4404b284
DESKTOP-JGJSHGL\bih <peroxide614@gmail.com>
2024-09-24 01:34:58 +0300

    add more icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,4 +2,7 @@
 /* eslint-disable padding-line-between-statements */
 export { default as ArrowLeftIcon } from './arrow-left.svg?react';
 export { default as ArrowRightIcon } from './arrow-right.svg?react';
+export { default as CartIcon } from './cart-icon.svg?react';
+export { default as HeartIcon } from './heart-icon.svg?react';
 export { default as HomeIcon } from './home.svg?react';
+export { default as WebsiteLogo } from './website-logo.svg?react';
