f65f9504f920cd4c83411624a4236cd2ae676bc8
DESKTOP-JGJSHGL\bih <peroxide614@gmail.com>
2024-09-24 01:35:20 +0300

    change breadcrumb class

--- src/components/Breadcrumb/Breadcrumb.jsx
+++ src/components/Breadcrumb/Breadcrumb.jsx
@@ -6,7 +6,7 @@ import { ArrowRightIcon, HomeIcon } from '../../assets';
 export const Breadcrumb = ({ currentName }) => {
   return (
     <nav className="flex gap-2 pb-7">
-      <Link to="/" className="breadcrumb-home">
+      <Link to="/" className="color-colorTextBase">
         <HomeIcon className="w-4 h-4" />
       </Link>
       <div className="text-colorGrey">
