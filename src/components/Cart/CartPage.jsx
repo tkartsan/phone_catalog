5f2fd33edb3cd7922dfd3f088e4121c84c42b600
DESKTOP-JGJSHGL\bih <peroxide614@gmail.com>
2024-10-02 20:13:36 +0300

    add cart fix

--- src/components/Cart/CartPage.jsx
+++ src/components/Cart/CartPage.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
-import { usePhoneStore } from '../../store'; // Import the store
+import { usePhoneStore } from '../../store';
 
 export const CartPage = () => {
-  const { cart, removeFromCart, updateCartQuantity } = usePhoneStore(); // Assuming updateCartQuantity will handle quantity updates in Zustand
+  const { cart, removeFromCart, updateCartQuantity } = usePhoneStore();
 
-  // Calculate the total price of the cart based on the item quantities
   const totalPrice = cart.reduce(
-    (acc, phone) =>
-      acc + (phone.priceDiscount || phone.priceRegular) * (phone.quantity || 1),
+    (acc, purchase) =>
+      acc +
+      (purchase.priceDiscount || purchase.priceRegular) *
+        (purchase.quantity || 1),
     0,
   );
   const totalItems = cart.reduce(
-    (acc, phone) => acc + (phone.quantity || 1),
+    (acc, purchase) => acc + (purchase.quantity || 1),
     0,
   );
 
@@ -21,53 +22,53 @@ export const CartPage = () => {
       <div className="flex flex-col mb-6">
         <h1 className="text-[48px] font-bold">Cart</h1>
       </div>
-
       {cart.length > 0 ? (
         <div className="flex flex-col lg:flex-row gap-6">
-          {/* Cart Items */}
           <div className="flex-1">
-            {cart.map((phone) => (
+            {cart.map((purchase) => (
               <div
-                key={phone.id}
+                key={purchase.id}
                 className="flex items-center justify-between border-solid border-colorDifferentGrey p-4 mb-4"
               >
                 <div className="flex items-center gap-4">
                   <button
-                    onClick={() => removeFromCart(phone.id)}
+                    onClick={() => removeFromCart(purchase.id)}
                     className="text-bgColorLightGrey hover:text-bgColorGrey"
                   >
-                    &#10005; {/* This is the "X" symbol */}
+                    &#10005;
                   </button>
                   <img
-                    src={`/${phone.images[0]}`}
-                    alt={phone.name}
+                    src={`/${purchase.images[0]}`}
+                    alt={purchase.name}
                     className="w-20 h-20 object-contain rounded-md"
                   />
                   <div>
                     <h3 className="font-mont text-[14px] leading-[21px] text-left">
-                      {phone.name}
+                      {purchase.name}
                     </h3>
                   </div>
                 </div>
                 <div className="flex items-center gap-6">
-                  {/* Quantity controls */}
                   <div className="flex items-center border px-3 py-1 rounded-md">
                     <button
                       className="text-gray-700 border-solid border-colorLightGrey w-8 h-8 justify-center"
                       onClick={() =>
                         updateCartQuantity(
-                          phone.id,
-                          Math.max((phone.quantity || 1) - 1, 1),
+                          purchase.id,
+                          Math.max((purchase.quantity || 1) - 1, 1),
                         )
                       }
                     >
                       -
                     </button>
-                    <span className="px-3">{phone.quantity || 1}</span>
+                    <span className="px-3">{purchase.quantity || 1}</span>
                     <button
                       className="text-gray-700 border-solid border-colorLightGrey w-8 h-8 justify-center"
                       onClick={() =>
-                        updateCartQuantity(phone.id, (phone.quantity || 1) + 1)
+                        updateCartQuantity(
+                          purchase.id,
+                          (purchase.quantity || 1) + 1,
+                        )
                       }
                     >
                       +
@@ -75,15 +76,13 @@ export const CartPage = () => {
                   </div>
                   <p className="text-xl font-bold">
                     $
-                    {(phone.priceDiscount || phone.priceRegular) *
-                      (phone.quantity || 1)}
+                    {(purchase.priceDiscount || purchase.priceRegular) *
+                      (purchase.quantity || 1)}
                   </p>
                 </div>
               </div>
             ))}
           </div>
-
-          {/* Cart Summary */}
           <div className="w-full lg:w-1/3">
             <div className="border-solid border-colorDifferentGrey p-6 ">
               <div className="flex flex-col align-center justify-center mb-3">
--- src/store/usePhoneStore.js
+++ src/store/usePhoneStore.js
@@ -4,45 +4,45 @@ import { persist } from 'zustand/middleware';
 export const usePhoneStore = create(
   persist(
     (set, get) => ({
-      // Cart management
       cart: [],
-
+      favorites: [],
       addToCart: (phone) =>
         set((state) => {
           const existsInCart = state.cart.some((item) => item.id === phone.id);
 
           if (!existsInCart) {
-            return { cart: [...state.cart, { ...phone, quantity: 1 }] }; // Start with quantity 1
+            return { cart: [...state.cart, { ...phone, quantity: 1 }] };
           }
 
-          return state; // No changes if phone is already in the cart
+          return state;
         }),
-
       removeFromCart: (phoneId) =>
         set((state) => ({
           cart: state.cart.filter((phone) => phone.id !== phoneId),
         })),
-
       isInCart: (phoneId) => get().cart.some((phone) => phone.id === phoneId),
+      updateCartQuantity: (phoneId, newQuantity) =>
+        set((state) => {
+          const updatedCart = state.cart.map((item) =>
+            item.id === phoneId ? { ...item, quantity: newQuantity } : item,
+          );
 
-      // Favorites management
-      favorites: [],
-
+          return { cart: updatedCart };
+        }),
       addFavorite: (phone) =>
         set((state) => ({
           favorites: [...state.favorites, phone],
         })),
-
       removeFavorite: (phoneId) =>
         set((state) => ({
           favorites: state.favorites.filter((phone) => phone.id !== phoneId),
         })),
-
       isFavorite: (phoneId) =>
         get().favorites.some((phone) => phone.id === phoneId),
     }),
+
     {
-      name: 'phone-store', // Local storage key for Zustand store
+      name: 'phone-store',
     },
   ),
 );
